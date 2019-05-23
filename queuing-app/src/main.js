357754c68f7739291a7aa7c21b2d66900ef547b6
Daniel Bencz <daniel.bencz@gmail.com>
2019-05-23 17:46:16 +0200

    begun with routing

--- queuing-app/src/main.js
+++ queuing-app/src/main.js
@@ -1,17 +1,26 @@
+'use strict';
 import Vue from 'vue'
 
 /* importing components, you name them here */
 import App from './App.vue'
 import Header from './Header.vue'
-/* use arbitrary name */
 import Gringos from '../src/nestedComp.vue'
 
 /* registering components globally */
 Vue.component('teso', Gringos);
 
+/* setting up routing */
+import VueRouter from 'vue-router'
+import Routes from './routes'
+Vue.use(VueRouter);
+const router = new VueRouter({
+  routes: Routes
+});
+
 const app = new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router: router
 })
 
 const header = new Vue({
---
+++ queuing-app/src/routes.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import userLogin from './components/login-user.vue';
+import providerLogin from './components/login-provider.vue';
+
+export default[
+  {path:'/userlogin', component: userLogin},
+  {path:'/providerlogin', component: providerLogin},
+  {path:'/userlogin', component: userLogin},
+  {path:'/userlogin', component: userLogin},
+ 
+]
