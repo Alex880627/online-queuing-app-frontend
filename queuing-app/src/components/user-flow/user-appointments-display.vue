3d1bac706fe88bc03c4249851c79bf20ebb79c90
Daniel Bencz <daniel.bencz@gmail.com>
2019-05-24 13:01:11 +0200

    added index

--- queuing-app/src/components/user-flow/user-appointments-display.vue
+++ queuing-app/src/components/user-flow/user-appointments-display.vue
@@ -4,7 +4,7 @@
     <h3>Booked Appointments</h3>
     <tbody>
       <tr v-for="(appointment, index) in appointments">
-        <td>{{index}}</td>
+        <td>{{index+1}}</td>
         <th>{{appointment.serviceName}}</th>
         <td>{{appointment.providerName}}</td>
         <td>{{appointment.appointmentDate}}</td>
@@ -22,8 +22,8 @@ export default {
   computed: {
     getAppointments() {
       console.log("yoyoyoyoyoyoy" + this.$store.state.userAppointments.data);
-      /* this.appointments = [...this.$store.state.userAppointments.data]; */
-      this.appointments = [...this.$store.state.databaseData.appointments_user];
+      this.appointments = [...this.$store.state.userAppointments.data];
+      /* this.appointments = [...this.$store.state.databaseData.appointments_user]; */
     }
   },
   methods: {
