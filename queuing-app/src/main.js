7cfe29101fef9f49124cffea1039bf83317f22eb
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-05-24 09:06:53 +0200

    nesze

--- queuing-app/src/main.js
+++ queuing-app/src/main.js
@@ -2,9 +2,7 @@
 
 import Vue from 'vue';
 import { store } from './store/store';
-import fetcherComponentDemo from './components/fetcherComponentDemo.vue';
 import App from './App.vue';
-//import Header from './Header.vue'
 import HeaderComponent from './components/headerComponent.vue';
 import ProviderSettings from './components/provider-flow/ProviderSettings.vue';
 import UserMainPage from './components/user-flow/user-main.vue';
@@ -12,11 +10,10 @@ import UserServicesDisplay from './components/user-flow/user-services-display.vu
 import UserAppointmentsDisplay from './components/user-flow/user-appointments-display.vue';
 
 /* registering comp/onents globally */
-Vue.component('fetcherComponentDemo',fetcherComponentDemo); //  what you name here can be imported inside everywhere
 Vue.component('userMainComp', UserMainPage);
 Vue.component('userServiceDisplay', UserServicesDisplay);
 Vue.component('userAppointmentsDisplay', UserAppointmentsDisplay);
-Vue.component('providerSettings', ProviderSettings)
+Vue.component('providerSettings', ProviderSettings);
 Vue.component('headerComponent',HeaderComponent);
 
 /* setting up routing */
@@ -36,8 +33,3 @@ const app = new Vue({
   render: h => h(App),
   router: router
 })
-/*
-const header = new Vue({
-  el: '#header',
-  render: h => h(Header)
-})*/
